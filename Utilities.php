3a7b6174afae794097315ca9ef67956e10280566
Dennis <dennistu1994@hotmail.com>
2013-03-11 10:12:24 -0400

    added setpage function to utilities class

--- Utilities.php
+++ Utilities.php
@@ -11,16 +11,24 @@ class Utilities
 	
 	}
 	
-	public static function RunPage($pagename)
+	public static function RunPage($filename)
 	{
 		$connection = mysql_connect("localhost","814459_jq2tu","Tjq@132508") or die(mysql_error());
 		mysql_select_db("rookiecodes_clanteam_project",$connection);
 
-		$query = 'select filecontent from PHP where filename = "'.$pagename.'"';
+		$query = 'select filecontent from PHP where filename = "'.$filename.'"';
 
 		$qresult = mysql_query($query,$connection) or die(mysql_error());
 		$result = mysql_fetch_row($qresult);
 		eval($result[0]);
+	}
+	
+	public static function SetPage($filename,$filecontent)
+	{
+		$connection = mysql_connect("localhost","814459_jq2tu","Tjq@132508") or die(mysql_error());
+		mysql_select_db("rookiecodes_clanteam_project",$connection);
+		$query = 'update PHP set filecontent = "'.$filecontent.'" where "filename = "'.$filename.'" '	
+		$qresult = mysql_query($query,$connection) or die(mysql_error());
 	}
 }
 ?>
